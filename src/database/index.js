a9a9e7af6e235abe0282c0c76169b610e4209454
LuizGustav0 <contato-luiz-gustavo@outlook.com>
2020-07-06 12:59:21 -0300

    Estrutura inicial de pastas

---
+++ src/database/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+module.exports = mongoose;
